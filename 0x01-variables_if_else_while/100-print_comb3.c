f3aa80028015acde2a49e51a67fcea6c2fb6e224
RayanAlkandagawy <arbabrayan513@gmail.com>
2022-12-10 00:21:40 -0800

    task 6

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - putchar alphabets
+ * main - Prints all possible combinations of two two-digit numbers,
+ *     ranging from 0-99, separated by a comma followed by a space.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -28,6 +29,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
