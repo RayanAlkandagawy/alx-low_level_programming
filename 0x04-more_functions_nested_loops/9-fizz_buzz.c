4721c16cc50bde378735685d19c447dc123c779f
RayanAlkandagawy <arbabrayan513@gmail.com>
2022-12-15 11:31:22 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - fizz buzz fizzbuzz stupid task
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+	char space;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i != 100)
+			space = ' ';
+		else
+			space = '\n';
+		if ((i % 15) == 0)
+			printf("FizzBuzz%c", space);
+		else if ((i % 5) == 0)
+			printf("Buzz%c", space);
+		else if ((i % 3) == 0)
+			printf("Fizz%c", space);
+		else
+			printf("%i%c", i, space);					}
+	return (0);
+}
